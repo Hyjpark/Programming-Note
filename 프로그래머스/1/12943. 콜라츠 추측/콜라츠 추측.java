87d54d804a290c87c9b4438c962a057aebef4bde
HYEJEONG PAK <42841712+Hyjpark@users.noreply.github.com>
2025-07-25 09:41:29 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int num) {
+        if (num == 1) return 0;
+        
+        long n = num;
+        int cnt = 0;
+        
+        while (n != 1) {
+            n = n % 2 == 0 ? n / 2 : (n * 3) + 1;
+            cnt++;
+            
+            if (cnt == 500) return -1;
+        }
+        
+        return cnt;
+    }
+}
