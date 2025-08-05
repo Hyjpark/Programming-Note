58e303c49f3fe5b59418429722acf130f01ca655
HYEJEONG PAK <42841712+Hyjpark@users.noreply.github.com>
2025-08-05 09:44:02 +0900

    [Bronze V] Title: 삼각형, Time: 176 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+        System.out.println((double) (w * h) / 2);
+    }
+}
