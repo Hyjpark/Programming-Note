a7fa2b269c445335aa7406cbc28bfef0f311da1d
HYEJEONG PAK <42841712+Hyjpark@users.noreply.github.com>
2025-08-05 10:21:31 +0900

    [Bronze IV] Title: 모비스, Time: 172 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/28074. 모비스/모비스.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        String m = "";
+        if (str.contains("M")
+            && str.contains("O")
+            && str.contains("B")
+            && str.contains("I")
+            && str.contains("S")) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
