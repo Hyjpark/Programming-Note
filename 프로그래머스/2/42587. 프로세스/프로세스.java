5bb7483fcb8dffa0e565e091745c0097777b4001
HYEJEONG PAK <42841712+Hyjpark@users.noreply.github.com>
2025-10-14 09:17:12 +0900

    [level 2] Title: 프로세스, Time: 1.25 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        
+    PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+    for (int num : priorities) {
+        priorityQueue.add(num);
+    }
+
+    while (!priorityQueue.isEmpty()) {
+        for (int i = 0; i < priorities.length; i++) {
+            if (priorities[i] == priorityQueue.peek()) {
+                priorityQueue.poll();
+                answer++;
+                if (i == location) {
+                    return answer;
+                }
+            }
+        }
+    }
+        
+        return answer;
+    }
+}
